1be6c253e0d49c6899ec2dda1e8ef439e71bc8ef
Ary Vidot <aryvid@gmail.com>
2020-05-23 00:25:49 +0100

    JS code added

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
-var randomNumber1 = Math.random();
-n = randomNumber1 * 6;
-n = Math.floor(n) + 1;
-console.log(n);
+// random Number 1
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; // 1-6
 
-document.setAttribute("n", [(src = "")]);
+var randomDiceImage = "dice" + randomNumber1 + ".png"; // dice1.png - dice6.png
 
-function randomNumber() {
-  var randomNumber1 = Math.floor(Math.random() * 6 + 1);
-  console.log(randomNumber1);
-  //   document.setAttribute("src", "images/");
+var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+// random Number 2
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomDiceImage2 = "images/dice" + randomNumber2 + ".png";
+
+var image2 = document
+  .querySelectorAll("img")[1]
+  .setAttribute("src", randomDiceImage2);
+
+//   Change Title accoring to Winner
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 1 Wins!";
+} else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins!";
+} else (randomNumber1 === randomNumber2) {
+  document.querySelector("h1").innerHTML = "Draw!";
 }
