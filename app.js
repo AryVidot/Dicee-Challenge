82a884beca76d1f89e86fc55ba1ba1e2d66a5391
Ary Vidot <aryvid@gmail.com>
2020-05-22 22:56:34 +0100

    new js code

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
-var randomNumber1 = Math.random() * 6;
+var randomNumber1 = Math.random();
 n = randomNumber1 * 6;
 n = Math.floor(n) + 1;
 console.log(n);
+
+document.setAttribute("n", [(src = "")]);
+
+function randomNumber() {
+  var randomNumber1 = Math.floor(Math.random() * 6 + 1);
+  console.log(randomNumber1);
+  //   document.setAttribute("src", "images/");
+}
